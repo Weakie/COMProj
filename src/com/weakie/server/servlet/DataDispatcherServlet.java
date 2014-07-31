9f8d92ff16d16457eee0064f676a4762f78d65ae
Weakie <weakielin@gmail.com>
2014-07-31 15:36:24 +0800

    refactor

--- src/com/weakie/server/servlet/DataDispatcherServlet.java
+++ src/com/weakie/server/servlet/DataDispatcherServlet.java
@@ -24,7 +24,7 @@ public class DataDispatcherServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 	/**

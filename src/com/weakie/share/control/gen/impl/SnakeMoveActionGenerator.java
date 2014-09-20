267adf97235886c4d3035c586c045928294414de
Weakie <weakielin@gmail.com>
2014-09-20 22:02:53 +0800

    new class on snake generator action

---
+++ src/com/weakie/share/control/gen/impl/SnakeMoveActionGenerator.java
@@ -0,0 +1,45 @@
+package com.weakie.share.control.gen.impl;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import com.weakie.share.control.gen.AbstractActionGenerator;
+import com.weakie.share.control.gen.ActionBean;
+
+public class SnakeMoveActionGenerator extends AbstractActionGenerator {
+
+	public SnakeMoveActionGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Iterator<ActionBean> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setParameter(Map<String, Object> value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescript() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
